0e181a0c250fa9de71c2a8a28a152ffa82ac6a1c
Kotov Alexandr <Mr1Hiroshi@yandex.ru>
2025-07-06 00:33:40 +0500

    exchangeController

--- routes/exchangeRoutes.js
+++ routes/exchangeRoutes.js
@@ -1,2 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const exchangeController = require('../controllers/exchangeController');
+
 router.post('/', exchangeController.sendRequest);
 router.get('/my', exchangeController.getMyRequests);
