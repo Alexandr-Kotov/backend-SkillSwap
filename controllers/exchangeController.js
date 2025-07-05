3ea609c2906df79b5d7537e49d3acca3fcff2c60
Kotov Alexandr <Mr1Hiroshi@yandex.ru>
2025-07-06 00:44:31 +0500

    getMyRequests

--- controllers/exchangeController.js
+++ controllers/exchangeController.js
@@ -1,4 +1,18 @@
 // controllers/exchangeController.js
+
+const ExchangeRequest = require('../models/ExchangeRequest');
+
+exports.getMyRequests = async (req, res) => {
+  try {
+    const requests = await ExchangeRequest.find({ to: req.user }).populate('from to', 'name email');
+    res.json(requests);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+
+
 exports.sendRequest = async (req, res) => {
   const { to, offerDetails } = req.body;
   const newReq = new ExchangeRequest({
